2a60894528298bf298a9b3fc8c79cbde110fbadf
benson <benson@Benson.local>
2018-03-15 16:30:53 +0800

    add: 引入模板

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const Koa = require('koa')
 const app = new Koa()
-const { normal } = require('./tpl/index')
+const { htmlTpl, ejsTpl, pugTpl } = require('./tpl/index')
+const ejs = require('ejs')
+const pug = require('pug')
+
 app.use(async (ctx, next) => {
   ctx.type = 'html'
-  ctx.body = normal
+  // ctx.body = htmlTpl
+
+  // ctx.body = ejs.render(ejsTpl, {
+  //   you: 'you',
+  //   me: 'Benson'
+  // })
+
+  ctx.body = pug.render(pugTpl, {
+    you: 'you',
+    me: 'Benson'
+  })
   await next()
 })
 
