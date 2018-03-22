087f70218e9332de6cd744557337bc4857ee2be6
benson <benson@Benson.local>
2018-03-22 10:58:26 +0800

    feat: 用import动态加载前端路由

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+// 前端路由
+import AC from './components/async_load'  // 动态加载
+
+export default [
+  {
+    name: '首页',
+    icon: 'home',
+    path: '/',
+    component: AC(() => import('./views/home'))
+  },
+  {
+    name: '详情页',
+    icon: 'detail',
+    path: '/detail/:id',
+    component: AC(() => import('./views/movie/detail'))
+  }
+]
